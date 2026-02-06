b5a71544e7cf8b2dc22342cafd7590ece4d65783
dev-abdullah11 <abdullahworld111@gmail.com>
2026-02-06 08:08:57 +0500

    fixing the logo missing issue

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -25,7 +25,7 @@ export default function Header() {
     
         <img
         onClick={()=>window.location.href="/"}
-        src="../../public/icon.jpg" alt="Quiz Logo" className="h-40 object-contain" />
+        src="/icon.jpg" alt="Quiz Logo" className="h-40 object-contain" />
 
     <div className=' flex gap-3.5'>
         <div
